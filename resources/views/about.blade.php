63ccf93d5ac22931f0244e6087a76868935382a8
unknown <kadekyudaarianta@gmail.com>
2023-04-18 12:48:25 +0700

    membuat halaman about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman About</title>
+  </head>
+  <body>
+    <h1>Halaman About</h1>
+  </body>
+</html>
